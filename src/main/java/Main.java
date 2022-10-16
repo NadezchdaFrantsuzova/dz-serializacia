e409621bfa40faf44bda927ccfac6dd0963a0121
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-10-16 21:52:55 +0300

    Добавлен файл chop.xml, в Main добавлена логика работы проекта в соответствии с файлом chop.xml, внесены изменения в файл .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,78 @@
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, SAXException,
+            ParserConfigurationException, XPathExpressionException {
         Scanner scanner = new Scanner(System.in);
 
         String[] product = {"Хлеб", "Молоко", "Соль", "Масло подсолнечное",
                 "Крупа гречневая", "Кефир", "Яйца", "Квас"};
         int[] prices = {100, 120, 60, 120, 90, 80, 90, 160};
-        Basket basket = new Basket(product, prices);
 
+        //shop.xml:
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse("chop.xml");
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String loadFileName = xPath
+                .compile("/config/load/fileName")
+                .evaluate(document); //basket.json
+        boolean loadEnabled = Boolean.parseBoolean(xPath
+                .compile("/config/load/enabled")
+                .evaluate(document)); //false
+        String loadFormat = xPath
+                .compile("/config/load/format")
+                .evaluate(document); //json
+        String saveFileName = xPath
+                .compile("/config/save/fileName")
+                .evaluate(document); //basket.json
+        boolean saveEnabled = Boolean.parseBoolean(xPath
+                .compile("/config/save/enabled")
+                .evaluate(document)); //true
+        String saveFormat = xPath
+                .compile("/config/save/format")
+                .evaluate(document);
+        String logFileName = xPath
+                .compile("/config/log/fileName")
+                .evaluate(document); //client.csv
+        boolean logEnabled = Boolean.parseBoolean(xPath
+                .compile("/config/log/enabled")
+                .evaluate(document)); //true
+
+
+        Basket basket = new Basket(product, prices);
         File textFile = new File("basket.txt");
         ClientLog clientLog = new ClientLog();
-        File txtFile = new File("log.scv");
-        File jsonFile = new File("basket.json");
+//        File txtFile = new File("log.scv");
+//        File jsonFile = new File("basket.json");
+        File txtFile = new File(logFileName);
+        File jsonFile = new File(saveFileName);
 
-        if (jsonFile.exists()) {
-            basket = Basket.loadJson(jsonFile);
-            System.out.println("Существует покупательская корзина:");
-            basket.printCart();
+        if (loadEnabled) {
+            if (loadFormat.equals("json")) {
+                if (jsonFile.exists()) {
+                    basket = Basket.loadJson(jsonFile);
+                    System.out.println("Существует покупательская корзина:");
+                    basket.printCart();
+                }
+            } else if (loadFormat.equals("txt")) {
+                if (textFile.exists()) {
+                    basket = Basket.loadFromTxtFile(textFile);
+                    System.out.println("Существует покупательская корзина:");
+                    basket.printCart();
+                }
+            }
         } else {
             System.out.println("Создайте покупательскую корзину:");
         }
@@ -27,6 +80,7 @@ public class Main {
         for (int i = 0; i < product.length; i++) {
             System.out.println((i + 1) + "." + product[i] + " - " + prices[i] + " руб./шт.");
         }
+
         int productNum;
         int amount;
 
@@ -44,10 +98,18 @@ public class Main {
                 basket.addToCart(productNum, amount);
             }
         }
-        basket.printCart();
-        basket.saveJson(jsonFile);
-        //basket.saveTxt(textFile);
 
-        clientLog.exportAsCSV(txtFile);
+        if (saveEnabled) {
+            if (saveFormat.equals("json")) {
+                basket.saveJson(jsonFile);
+            } else {
+                basket.saveTxt(textFile);
+            }
+        }
+        if (logEnabled) {
+            clientLog.exportAsCSV(txtFile);
+        }
+
+        basket.printCart();
     }
 }
